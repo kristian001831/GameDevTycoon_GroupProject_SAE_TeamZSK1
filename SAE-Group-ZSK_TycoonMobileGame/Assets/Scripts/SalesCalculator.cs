16c4c8903ee3f135bf8c2458e28bcff668aa49c1
Kristian <hisfh67@gmail.com>
2020-10-04 18:13:43 +0200

    .

---
+++ SAE-Group-ZSK_TycoonMobileGame/Assets/Scripts/SalesCalculator.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using Unity.Profiling;
+using UnityEngine;
+
+
+public class SalesCalculator
+{
+    public float Quality { get; set; }
+    public float PriceOfProduct { get; set; }
+    public float PriceOfAdds { get; set; }
+    public float MagnitudeOfQuality { get; set; }
+    public bool  PaidForAds { get; set; }
+
+    public float TrendScaler { get; set; }
+    public float PriceToPurchaseRateScaler { get; set; }
+    public float AdvertisementScaler { get; set; }
+    public float InterestFallOffScaler { get; set; }
+
+
+    //clamp every function to 0
+
+
+    public float CopiesSoldByDayX(float xDaysSinceRelease) //
+    {
+        float result = 1;
+
+        result *= Mathf.Lerp(1,TrendFunction(xDaysSinceRelease),TrendScaler);
+
+        result *= Mathf.Lerp(1,PriceToPurchase(Quality, PriceOfProduct),PriceToPurchaseRateScaler);
+
+        result *= Mathf.Lerp(1,Advertisement(PriceOfAdds, PaidForAds),AdvertisementScaler);
+       // UnityEngine.Debug.Log(result);
+
+        result *= Mathf.Lerp(1,InterestFalloff(xDaysSinceRelease, Quality, MagnitudeOfQuality),InterestFallOffScaler);
+
+  
+        return result;
+    }
+
+    private float TrendFunction(float xDays)
+    {
+        //todo: create a gendre for g
+
+        int a = 5;
+        int b = 20;
+        int c = 50;
+
+        float trendValue = Mathf.Sin(xDays / a) + Mathf.Sin(xDays / b) + Mathf.Sin(xDays / c) + 1;
+
+
+        return trendValue;
+
+        // Trend(g, x) = sin(x / a) + sin(x / b) + sin(x / c) + 1 
+    }
+
+
+    private float PriceToPurchase(float q, float p)
+    {
+        float priceOfProduct = ((-1) * (Mathf.Sqrt(p * 0.2f)) + Mathf.Pow((1 + q), 2));
+
+        return priceOfProduct;
+        //Price(p, q) = -sqrt(p * 0.2) + (1 + q) ^ 2
+    }
+
+
+    private float Advertisement(float adsPrice, bool paidForAdds)
+    {
+        
+        if (paidForAdds == true)
+        {
+           return ((-1) * (1 / adsPrice) + 1);
+        }
+        else
+        {
+           return  1;
+        }
+
+        //A(i) = -1/i +1
+    }
+
+    private float InterestFalloff(float xDays, float q, float m)
+    {
+        float interestFallOff = ((100 / xDays) + (q * m));
+
+        return interestFallOff;
+        // InterestFalloff(x) = 100/x + q*m
+        // m is the magnitue of the quality
+    }
+    /*
+     * F(x) = Trend(g,x) * PriceToPurchase(p,q) * Advertisement(i) * InterestFalloff(x,q)
+     * 
+     * F = Quantity sold on day x
+     * F(0) = release day
+     * q  = Quality (range 0-1)
+     * p = Price 
+     * x = days since release
+     * i = money paid for ads
+     * 
+     * 
+     * InterestFalloff(x) = 100/x + q*m
+     * 
+     * 
+     * Trend(g,x) = sin(x/a) + sin(x/b) + sin(x/c) +1 
+     * 
+     * where a,b,c are random int >0
+     * 
+     * Price(p,q) = -sqrt(p*0.2) + (1+q)^2
+     * 
+     * 
+     * 
+     * 
+     */
+}
---
+++ SAE-Group-ZSK_TycoonMobileGame/Library/Collab/Base/Assets/Scripts/ProductCreation/CreateProduct.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using System.Security.Cryptography;
+using System.Collections.Specialized;
+
+public class CreateProduct : MonoBehaviour
+{
+    [SerializeField] private GameObject _createObjectUI;
+    [SerializeField] private RectTransform _addButton;
+    [SerializeField] private ProductsHolder _list;
+    [SerializeField] private GameObject _productPrefab;
+
+    [SerializeField] private TMP_InputField _name;
+    [SerializeField] private TMPro.TMP_Dropdown _genre;
+    [SerializeField] private TMP_InputField _price;
+    [SerializeField] private TMP_InputField _invest;
+
+    private Product _productInfo;
+    private RectTransform _display;
+
+    public void CreateAProduct()
+    {
+         GameObject obj = Instantiate(_productPrefab);
+        obj.name = _name.text;
+
+        _productInfo = obj.GetComponent<Product>();
+        _productInfo.Name = _name.text;
+        _productInfo.Genre = (Product.EGenre)_genre.value;
+
+        if (float.TryParse(_price.text, out _productInfo.Price))
+        {
+            _productInfo.Price = float.Parse(_price.text);
+            double tempP = (double)Math.Round(_productInfo.Price, 2);
+            _productInfo.Price = (float)tempP;
+        }
+        else
+        {
+            _productInfo.Price = 0.0f;
+        }
+
+        if (float.TryParse(_invest.text, out _productInfo.InvestedAmount))
+        {
+            _productInfo.InvestedAmount = float.Parse(_invest.text);
+            double tempI = (double)Math.Round(_productInfo.InvestedAmount, 2);
+            _productInfo.InvestedAmount = (float)tempI;
+        }
+        else
+        {
+            _productInfo.InvestedAmount = 0.0f;
+        }
+
+        _list.Products.Add(obj);
+        obj.transform.parent = _list.gameObject.transform;
+
+        obj.transform.position = new Vector3(_list.gameObject.transform.position.x, _list.gameObject.transform.position.y, _list.gameObject.transform.position.z);
+        obj.transform.eulerAngles = new Vector3(_list.gameObject.transform.eulerAngles.x, _list.gameObject.transform.eulerAngles.y, _list.gameObject.transform.eulerAngles.z);
+        obj.transform.localScale = new Vector3(1, 1, 1);
+
+        _display = obj.GetComponentInChildren<RectTransform>();
+        _display.localPosition = new Vector3(0, 250 - 440 * (_list.Products.Count - 1), _display.localPosition.z);
+
+        _addButton.localPosition = new Vector3(-600, 380 - 440 * _list.Products.Count, _addButton.localPosition.z);
+
+        _createObjectUI.SetActive(false);
+    }
+}
