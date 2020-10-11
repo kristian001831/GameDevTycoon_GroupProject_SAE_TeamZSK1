e0e4f859079c6a08ca53fade476efc0f9b7e1ac1
Kristian <hisfh67@gmail.com>
2020-10-12 00:12:15 +0200

    begon with the time system and added a HideInspector script

--- SAE-Group-ZSK_TycoonMobileGame/Assets/Scripts/Time/TimeSystem.cs
+++ SAE-Group-ZSK_TycoonMobileGame/Assets/Scripts/Time/TimeSystem.cs
@@ -10,6 +10,8 @@ using UnityEngine;
 
 public class TimeSystem : MonoBehaviour
 {
+    //TODO: when we add some effects popping up etc, we need to add Time.DeltaTime;
+    
     // Days, Years Played
     private const string playerPrefNameDaysPlayedTotal = "DaysPlayedTotal";
     [SerializeField] private TextMeshProUGUI daysPlayedTotalText;
@@ -20,16 +22,15 @@ public class TimeSystem : MonoBehaviour
     [ReadOnlyInInspector.ReadOnly][SerializeField] private float daysPlayedTotal;
 
     [ReadOnlyInInspector.ReadOnly][SerializeField] private float timeCurrentDay;
-
-    [ReadOnlyInInspector.ReadOnly][SerializeField] private int timeMultiplicator;
+    
     [HideInInspector] public int TimeMultiplicator { get => timeMultiplicator; set => timeMultiplicator = value; }// Maybe delete later?
 
-    [ReadOnlyInInspector.ReadOnly][SerializeField] private int currentTimeMultiplicator;
+    [ReadOnlyInInspector.ReadOnly][SerializeField] private int timeMultiplicator;
+    [SerializeField] private int currentTimeMultiplicator = 1;
 
 
     void Start()
     {
-        
         // SavedTimePlayed is the value where the saved value is from, TODO: change it from playerprefs to json or so
         if (PlayerPrefs.HasKey(playerPrefNameDaysPlayedTotal))
         {
@@ -42,24 +43,51 @@ public class TimeSystem : MonoBehaviour
 
     void FixedUpdate()
     {
-        TimePase(timeMultiplicator);
-    }
-
-    public void ChangeTimeMultiplicator(int timeMultiplicator)
-    {
+        TimePass(currentTimeMultiplicator);
         
+        ShowTimeOnUI();
     }
 
-    private void TimePase(int multiplicator) // x0(pause), x1, x2, x3
+    public void ChangeTimeMultiplicator(int voidTimeMultiplicator)
+    {
+        currentTimeMultiplicator = voidTimeMultiplicator;
+    }
+
+    private void TimePass(int multiplicator) // x0(pause), x1, x2, x3
     {
         StartCoroutine(IOneMinute(multiplicator));
-        
-        
+    }
+
+    void ShowTimeOnUI()
+    {
+        daysPlayedTotalText.SetText("{0} days played", daysPlayedTotal);
+        timeCurrentDayText.SetText("{0}", timeCurrentDay);
     }
     
-    IEnumerator IOneMinute(int timeMultiplicator) 
+    IEnumerator IOneMinute(int _timeMultiplicator)
     {
-        yield return new WaitForSecondsRealtime(timeMultiplicator); //Wait 1 ingame min
-        Debug.Log("1 in game min is over.");
+        int x = 0;// x seconds = 1 hour
+        
+        if (_timeMultiplicator == 0)
+            x = 0;
+        else if (_timeMultiplicator == 1)
+            x = 9;
+        else if (_timeMultiplicator == 2)
+            x = 2;
+        else if (_timeMultiplicator == 3)
+            x = 1;
+        
+        // Speed x1: 9sec = 1hour in game, Speed x2: 2sec = 1hour in game, speed x3: 1sec = 1hour in game
+        yield return new WaitForSecondsRealtime(x); //Wait 1 ingame hour
+        Debug.Log("1 in game hour is over.");
+
+        if (timeCurrentDay <= 23)
+        {
+            timeCurrentDay += 1;
+        }
+        else if (timeCurrentDay == 24)
+        {
+            daysPlayedTotal += 1;
+        }
     }
 }
